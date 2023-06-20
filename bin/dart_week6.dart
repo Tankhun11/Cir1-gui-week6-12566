19feff736e6e08ea7de5627a9475c7d08b851b43
tankhun <tankoon300950@gmail.com>
2023-06-20 12:42:48 +0700

    first app

---
+++ bin/dart_week6.dart
@@ -0,0 +1,10 @@
+void main(){
+  int x = 10;
+  double y = 5;
+  print("Arithemetic x + y : $x + $y = ${x + y}");//1.Addition
+  print("Arithemetic x - y : $x - $y = ${x - y}");//2.Subtraction
+  print("Arithemetic x * y : $x * $y = ${x * y}");//3.Multi
+  print("Arithemetic x / y : $x / $y = ${x / y}");//4.Division 
+  print("Arithemetic x ~/y : $x ~/$y = ${x ~/y}");//5.Modulus
+  print("Arithemetic x % y : $x % $y = ${x % y}");//6.Modulus
+}
