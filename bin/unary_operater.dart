896468ede6ab0a81d629cf9c849bb57262e766c8
tankhun <tankoon300950@gmail.com>
2023-06-20 13:14:54 +0700

    update

--- bin/unary_operater.dart
+++ bin/unary_operater.dart
@@ -1,7 +1,17 @@
 void main(){
   int x = 10;
   int y = 5;
+  int z = 20;
+  int u = 25;
+  print(x++);//postfix value = 10 : 10 = 10 + 1
+  print(x++);//postfix value = 11 : 11 = 10 + 1
 
-  print(x++);//postfix value: x(10) = x(10+1)
-  print(x++);//postfix value: 11 = 10 + 1
+  print(++y);//prefix value = 6 : 5 = 5 + 1
+  print(++y);//prefix value = 7 : 7 = 6 + 1
+
+  print(z--);//prefix value = 20 : 20 = 20 - 1
+  print(z--);//prefix value = 19 : 19 = 20 - 1
+
+  print(--u);//prefix value = 24 : 24 = 25 - 1
+  print(--u);//prefix value = 23 : 23 = 24 - 1
 }
